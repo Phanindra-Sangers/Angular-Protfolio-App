5dba299bedfd61051035ae8fdf0fa6962df422b8
Your Name <you@example.com>
2024-01-14 14:53:44 +0530

    x

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -4,7 +4,7 @@ export class Tag {
     static readonly TERRAFORM = new Tag('Terraform','darkred');
     static readonly KUBERNETES = new Tag('Kubernetes','red');
     static readonly AWS = new Tag('Aws','orange');
-    static readonly CONTAINERS = new Tag('containers','red');
+    static readonly CONTAINERS = new Tag('containers','green');
     static readonly DEVOPS = new Tag('DevOps','blue');
     static readonly DOCKER = new Tag('Docker','yellow');
     private constructor(private readonly key: string, public readonly color: string){
